01a426be85504539a7338199d44b2ee17d5afc8a
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-05-25 09:54:34 +0100

    Update PyCaret_Streamlit_corrosion_prediction_less_data_batch.py

--- PyCaret_Streamlit_corrosion_prediction_less_data_batch.py
+++ PyCaret_Streamlit_corrosion_prediction_less_data_batch.py
@@ -107,14 +107,14 @@ def run():
         if st.button('Predict corrosion rate'):
             output = predict_corrosion(model, features_df)
             
-            if output < 1:
-                st.success("Corrosion risk: Low")
-            if output>=1 and output<5:
-                st.success("Corrosion risk: Moderate")
-            if output>=5 and output<10:
-                st.success("Corrosion risk: High")
-            if output >= 10:
-                st.success("Corrosion risk: Severe")
+            #if output < 1:
+                #st.success("Corrosion risk: Low")
+            #if output>=1 and output<5:
+                #st.success("Corrosion risk: Moderate")
+            #if output>=5 and output<10:
+                #st.success("Corrosion risk: High")
+            #if output >= 10:
+                #st.success("Corrosion risk: Severe")
                     
             output1=str("%.2f" % output) + ' mpy'
             
