3643b02f5f4d87a2515a1656a63d8680e4eae8e6
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-05-25 10:12:34 +0100

    Update PyCaret_Streamlit_corrosion_prediction_less_data_batch.py

--- PyCaret_Streamlit_corrosion_prediction_less_data_batch.py
+++ PyCaret_Streamlit_corrosion_prediction_less_data_batch.py
@@ -41,10 +41,10 @@ def predict_corrosion(model, df):
 def run():
     
     add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
+    "How would you like to predict corrosion rate?",
     ("Individual", "Batch"))
 
-    st.sidebar.info('This is a web app to predict corrosion rates of oil wells based on         several features that you can see in the sidebar. Please adjust the         value of each feature. After that, click on the Predict button at the bottom to         see the prediction of the model.')
+    st.sidebar.info('This is a web app to predict corrosion rates of oil wells based on         several features that you can see in the main body. Please adjust the         value of each feature. After that, click on the Predict button at the bottom to         see the prediction of the model.')
     
     st.sidebar.success('https://www.pungoapp.com')
     
