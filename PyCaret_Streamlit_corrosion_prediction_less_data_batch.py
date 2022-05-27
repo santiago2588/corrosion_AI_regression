b61f78477d51130f292619e7d77ff3db1300a3f9
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-05-27 22:17:47 +0100

    Update PyCaret_Streamlit_corrosion_prediction_less_data_batch.py

--- PyCaret_Streamlit_corrosion_prediction_less_data_batch.py
+++ PyCaret_Streamlit_corrosion_prediction_less_data_batch.py
@@ -50,6 +50,8 @@ def to_excel(df):
 
 def run():
     
+    st.sidebar.image("C:\Users\saint\Code examples\Corrosion AI\logo_pungo.png")
+    
     add_selectbox = st.sidebar.selectbox(
     "How would you like to predict corrosion rate?",
     ("Individual", "Batch"))
