43e8563b4055d144e833fe786270ced2b848adfa
Santiago <santiago.zapata2588@gmail.com>
2023-08-01 17:05:51 +0100

    Added text

--- pages/02_Prediccion_1_pozo.py
+++ pages/02_Prediccion_1_pozo.py
@@ -29,7 +29,7 @@ model = load_model('AI_models/corrosion_regressor_less_data')
 
 def predict_corrosion(model, df):
     predictions_data = predict_model(estimator = model, data = df)
-    predictions=predictions_data['Label'][0]
+    predictions=predictions_data['prediction_label'][0]
     return predictions
 
 def to_excel(df):
