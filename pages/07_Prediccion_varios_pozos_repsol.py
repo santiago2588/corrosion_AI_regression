555e41b83666b4ae2741842eb5d9b121f0d7470d
Santiago <santiago.zapata2588@gmail.com>
2023-08-22 16:49:09 +0100

    Added text

--- pages/07_Prediccion_varios_pozos_repsol.py
+++ pages/07_Prediccion_varios_pozos_repsol.py
@@ -57,7 +57,7 @@ def run():
     if file_upload is not None:
         data = pd.read_csv(file_upload)
         predictions = predict_model(estimator=model,data=data)
-        predictions=predictions.rename({'Label':'Corrosion_rate_mpy'},axis='columns')
+        predictions=predictions.rename({'prediction_label':'Corrosion_rate_mpy'},axis='columns')
         st.write(predictions)
 
         def convert_df(df):
